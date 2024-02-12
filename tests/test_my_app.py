b2d0e210a81bbe5f1d2a64da73eade0d3ba52b17
rameez <rameeznkhan@gmail.com>
2024-02-13 01:20:41 +0530

    initial commit

---
+++ tests/test_my_app.py
@@ -0,0 +1,12 @@
+import unittest
+from my_app import add
+
+class TestMyApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1,2),3)
+    
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1,-2), -3);
+
+if __name__=='__main__':
+    unittest.main()
